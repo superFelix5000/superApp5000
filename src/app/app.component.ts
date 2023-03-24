b16796a4e8905e882a3a31a813a17f5c5fa43d7d
Felix <fwest@posteo.de>
2023-03-24 20:28:05 +0200

    added tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-    title = 'superApp5000';
-}
+export class AppComponent {}
