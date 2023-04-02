e826b8e4bd9e34661d1a11ea1eaf6a5129fd886e
Felix <fwest@posteo.de>
2023-04-02 22:35:30 +0300

    tests for serviec

--- src/app/services/photos.service.spec.ts
+++ src/app/services/photos.service.spec.ts
@@ -1,62 +1,43 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
 import { PhotosService } from './photos.service';
+import {
+    HttpMethod,
+    SpectatorHttp,
+    createHttpFactory,
+} from '@ngneat/spectator';
 
 describe('PhotosService', () => {
-    let service: PhotosService;
+    let spectator: SpectatorHttp<PhotosService>;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
-        });
-        service = TestBed.inject(PhotosService);
+    const createHttp = createHttpFactory(PhotosService);
+
+    beforeEach(() => (spectator = createHttp()));
+
+    it('should call getAll with the correct URL', () => {
+        spectator.service.getAll().subscribe();
+        spectator.expectOne(
+            `${spectator.service['apiUrlPhotos']}`,
+            HttpMethod.GET
+        );
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('should call getPage with the correct URL and params', () => {
+        const pageNr = 5;
+        const limit = 20;
+        spectator.service.getPage(pageNr, limit).subscribe();
+        const req = spectator.expectOne(
+            `${spectator.service['apiUrlPhotos']}?_limit=20&_page=5`,
+            HttpMethod.GET
+        );
+        expect(req.request.params.get('_limit')).toEqual(limit.toString());
+        expect(req.request.params.get('_page')).toEqual(pageNr.toString());
+    });
+
+    it('should call getPhoto with the correct URL and id', () => {
+        const id = 1000;
+        spectator.service.getPhoto(id).subscribe();
+        spectator.expectOne(
+            `${spectator.service['apiUrlPhotos']}/${id}`,
+            HttpMethod.GET
+        );
     });
 });
-
-/**
- * describe('DataService', () => {
-  let spectator: SpectatorHttp<DataService>;
-  const createHttp = createHttpFactory(DataService);
-
-  beforeEach(() => spectator = createHttp());
-
-  it('getArticles: should call HttpClient.get with the correct URL', () => {
-    spectator.service.getArticles().subscribe();
-    spectator.expectOne('api/articles', HttpMethod.GET);
-  });
-
-  it('getArticle: should call HttpClient.get with the correct URL', () => {
-    spectator.service.getArticle('some-id').subscribe();
-
-    spectator.expectOne('api/articles/some-id', HttpMethod.GET);
-  });
-
-  it('addArticle: should call HttpClient.post with the correct URL and request body', () => {
-    const article: any = { id: 'some-id' };
-    spectator.service.addArticle(article).subscribe();
-
-    const req = spectator.expectOne('api/articles', HttpMethod.POST);
-    expect(req.request.body).toEqual(article);
-  });
-
-  it('updateArticle: should call HttpClient.put with the correct URL and request body', () => {
-    const article: any = { id: 'some-id' };
-    spectator.service.updateArticle(article).subscribe();
-
-    const req = spectator.expectOne('api/articles', HttpMethod.PUT);
-    expect(req.request.body).toEqual(article);
-  });
-
-  it('deleteArticle: should call HttpClient.delete with the correct URL', () => {
-    spectator.service.deleteArticle('some-id').subscribe();
-
-    spectator.expectOne('api/articles/some-id', HttpMethod.DELETE);
-  });
-
-});
- */
